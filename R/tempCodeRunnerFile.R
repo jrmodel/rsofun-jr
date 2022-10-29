1ba13e5de436752826a89b18790693ddacba3108
jrmodel <wujiarui822@gmail.com>
2022-10-29 22:30:32 +0200

    run the add_calib_param

--- R/tempCodeRunnerFile.R
+++
@@ -1,8 +0,0 @@
-    par = c(
-      as.numeric(params_modl$kphio),
-      as.numeric(params_modl$soilm_par_a),
-      as.numeric(params_modl$soilm_par_b),
-      as.numeric(params_modl$tau_acclim_tempstress),
-      as.numeric(params_modl$par_shape_tempstress)
-      as.numeric(params_modl$whc)
-      )
